49ba9c0252e55be5c8a06eda07974fbdcd246ba9
Mateusz Sikorski <mjo.sikorski@gmail.com>
2022-06-13 19:32:01 +0200

    Add fork move

--- Assets/Scripts/ForklifterController.cs
+++ Assets/Scripts/ForklifterController.cs
@@ -14,6 +14,12 @@ namespace Forklifter
 		[SerializeField] private float turnSpeed;
 		[SerializeField] private float backRotation;
 
+		[Header("Lift")]
+		[SerializeField] private Transform fork;
+		[SerializeField] private float minFrokPos;
+		[SerializeField] private float maxFrokPos;
+		[SerializeField] private float forkSpeed;
+
 		private float rotation;
 
 		private void Start()
@@ -25,29 +31,48 @@ namespace Forklifter
 		{
 			var vertical = Input.GetAxis("Vertical");
 			var horizontal = Input.GetAxis("Horizontal");
+			var dir = 0f;
+
+			if (Input.GetKey(KeyCode.PageDown))
+				dir = -1f;
+			if (Input.GetKey(KeyCode.PageUp))
+				dir = 1f;
 
 			Move(vertical, horizontal);
+			MoveFork(dir);
 		}
 
 		public void Move(float vertical, float horizontal)
 		{
+			float time = Time.fixedDeltaTime;
+
 			if (horizontal > 0.2f || horizontal < -0.2f)
 			{
-				rotation += turnSpeed * horizontal * Time.deltaTime;
+				rotation += turnSpeed * horizontal * time;
 				rotation = Mathf.Clamp(rotation, -maxRotation, maxRotation);
 			}
 			else
 			{
-				rotation = Mathf.Lerp(rotation, 0f, Time.deltaTime * backRotation);
+				rotation = Mathf.Lerp(rotation, 0f, time * backRotation);
 			}
 
 			if (vertical > 0.2f || vertical < -0.2f)
 			{
-				forkLifterRigidbody.angularVelocity = new Vector3(0, rotation * vertical * Time.deltaTime, 0);
+				forkLifterRigidbody.angularVelocity = new Vector3(0, rotation * vertical * time, 0);
 
 				var speed = forklifterSpeed * vertical;
 				forkLifterRigidbody.velocity = forklifterTransform.forward * speed;
 			}
+		}
+
+		public void MoveFork(float dir)
+		{
+			var forkPosition = fork.localPosition;
+
+			forkPosition.y += forkSpeed * Time.fixedDeltaTime * dir;
+			forkPosition.y = Mathf.Clamp(forkPosition.y, minFrokPos, maxFrokPos);
+
+			fork.localPosition = forkPosition;
 		}
 	}
 }
